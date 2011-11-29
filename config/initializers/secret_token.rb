b79db55281ffb46c595b31a14ff377e78fb94234
Krzysztof Urban <krzysztof.urban@netguru.pl>
2011-11-29 20:43:38 +0100

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halny::Application.config.secret_token = 'c36194f94a9a306a0f48f02d290012395fa2855dc0f23b1f93d859cb998c68ea9554d0b2368dd683b7e7f03b721ce94ffc25381bbd9a69708629792e7e165b24'
