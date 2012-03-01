10ff52b883c243d16a7d6bb0abd2587323878ffc
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-01 19:50:07 +0100

    adding and editing races done

--- app/controllers/admin/races_controller.rb
+++ app/controllers/admin/races_controller.rb
@@ -2,7 +2,45 @@
 
 class Admin::RacesController < AdminController
 
+  before_filter :find_race, only: [:edit, :update, :destroy]
+
   def index
     @races = Race.all
+  end
+
+  def new
+    @race = Race.new
+  end
+
+  def create
+    @race = Race.new(params[:race])
+
+    if @race.save
+      redirect_to admin_races_path, notice: "Dodano nowy rajd."
+    else
+      flash[:error] = "Popraw błędy w formularzu."
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @race.update_attributes(params[:race])
+      redirect_to admin_races_path, notice: "Zmiany zapisane."
+    else
+      flash[:error] = "Popraw błędy w formularzu."
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_race
+    @race = Race.find(params[:id])
   end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 class Race < ActiveRecord::Base
 
   scope :open_for_registration,
@@ -5,4 +7,19 @@ class Race < ActiveRecord::Base
 
   has_many :race_variants
 
+  validates_presence_of :name, :description, :registration_open_from, :registration_closed_at
+
+  validate :registration_open_before_close
+
+  def registrations_total
+    RaceRegistration.where(:race_variant_id => race_variant_ids).count
+  end
+
+  private
+
+  def registration_open_before_close
+    if registration_closed_at.present? && registration_open_from.present?
+      errors.add(:registration_closed_at, 'musi być datą po otwarciu rejestracji') if registration_closed_at <= registration_open_from
+    end
+  end
 end
