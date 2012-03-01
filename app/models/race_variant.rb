6f9989fe185eb6c331d6152dd4a7359db7ecb502
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-01 20:51:05 +0100

    create/update race_variants in admin + validations

--- app/models/race_variant.rb
+++ app/models/race_variant.rb
@@ -5,6 +5,9 @@ class RaceVariant < ActiveRecord::Base
   belongs_to :race
   has_many :race_registrations, dependent: :destroy
 
+  validates_presence_of :name, :description, :capacity
+  validates_numericality_of :capacity, only_integer: true
+
   def left
     capacity - race_registrations.count
   end
