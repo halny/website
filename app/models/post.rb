4336c281b5f101ffe4d9695c33a8d18257aa906a
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-02 23:53:37 +0100

    admin posts index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
 
   scope :published, lambda { where('published_at IS NOT NULL').where('published_at <= ?', Time.now).order('published_at DESC') }
+
+  def visible?
+    published_at.present? && published_at <= Time.now
+  end
 end
