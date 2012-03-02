8009e6e965435b3f312b9655bf89b395f47e19fd
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-02 23:26:44 +0100

    posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,7 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
+  def index
+    @posts = Post.published
   end
 
   # GET /posts/1
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+
+  scope :published, lambda { where('published_at IS NOT NULL').where('published_at <= ?', Time.now).order('published_at DESC') }
 end
