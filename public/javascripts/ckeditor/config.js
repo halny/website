b7c02d0ea886047f516b180553504db6a55b8442
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-03 02:07:38 +0100

    removed unnecessary buttons from ckeditor toolbar

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -56,10 +56,10 @@ CKEDITOR.editorConfig = function( config )
         ['Source','-','Preview'],
         ['Cut','Copy','Paste','PasteText','PasteFromWord',],
         ['Undo','Redo','-','SelectAll','RemoveFormat'],
-        ['Styles','Format'], ['Subscript', 'Superscript', 'TextColor'], ['Maximize','-','About'], '/',
+        ['Styles','Format'], ['Subscript', 'Superscript', 'TextColor'], ['Maximize'], '/',
         ['Bold','Italic','Underline','Strike'], ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
         ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['Link','Unlink','Anchor'], ['Image', 'Attachment', 'Flash', 'Embed'],
-        ['Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+        ['Link','Unlink','Anchor'], ['Image', 'Embed'],
+        ['Table','HorizontalRule','SpecialChar','PageBreak']
     ];
 };
