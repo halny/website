1c8715d338debe713ed983589932a920ff87a08a
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-03-03 01:58:18 +0100

    fix posts rendering

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
 
   scope :published, lambda { where('published_at IS NOT NULL').where('published_at <= ?', Time.now).order('published_at DESC') }
 
+  validates_presence_of :title, :content
+
   def visible?
     published_at.present? && published_at <= Time.now
   end
